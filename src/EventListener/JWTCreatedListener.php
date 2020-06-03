76dde64c2641af9bb37d51a3cce7829c5d66a95b
Roel <roel.van.bilzen@gmail.com>
2020-06-03 18:55:50 +0200

    Adding info to JWT

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $em)
+    {
+        $this->requestStack = $requestStack;
+        $this->em = $em;
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     * @return void
+     * Adding payload to the JWT when it is created
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        $userName = $event->getUser()->getUsername();
+
+        /**
+         * @var User $user
+         * Retrieving the user from the database by it's email
+         */
+        $user = $this->em->getRepository(User::class)->findOneBy(['email' => $userName]);
+
+        /**
+         * Adding different information to the JWT
+         */
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+        $payload['firstName'] = $user->getFirstName();
+        $payload['lastName'] = $user->getLastName();
+        $payload['ip'] = $request->getClientIp();
+
+        /**
+         * Payload is set for JWT
+         */
+        $event->setData($payload);
+
+//        $header        = $event->getHeader();
+//        $header['cty'] = 'JWT';
+//
+//        $event->setHeader($header);
+    }
+}
